c737e4677b971df774b418342dde671cbe338ff9
alexavier arias <alexavierga@gmail.com>
2021-10-24 17:21:20 -0400

    added app icon file

---
+++ 0tkinter.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+from tkinter import *
+#from tkinter.constants import *
+tk = Tk()
+frame = Frame(tk, relief=RIDGE, borderwidth=2)
+frame.pack(fill=BOTH,expand=1)
+label = Label(frame, text="Hello, World")
+label.pack(fill=X, expand=1)
+button = Button(frame,text="Exit",command=tk.destroy)
+button.pack(side=BOTTOM)
+tk.mainloop()
